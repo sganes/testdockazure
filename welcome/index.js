569565864d45ec652a96dddfb7dbc9efd653e0bf
shreeganes <shreevidhya.ganesan@byon8.com>
2018-11-09 09:50:53 +0100

    changes welcome msg

--- welcome/index.js
+++ welcome/index.js
@@ -1,16 +1,8 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+    const hostname = process.env.hostname || 'localhost'
+    msg = `Hello, connection string is mongodb://${hostname}:27017/tododb`;
+    context.res = {
+        status: 400,
+        body: msg
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Hello anonymous person"
-        };
-    }
-};
+}
