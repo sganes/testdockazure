36946cdac9cc82d264e04cc5765778e3b4797981
shreeganes <shreevidhya.ganesan@byon8.com>
2018-11-09 13:08:36 +0100

    changes in connection string

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM mcr.microsoft.com/azure-functions/node:2.0
-
 ENV AzureWebJobsScriptRoot=/home/site/wwwroot
 COPY . /home/site/wwwroot
--- db/mongoose_connect.js
+++ db/mongoose_connect.js
@@ -1,7 +1,5 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const hostname = process.env.hostname || 'localhost'
-mongoose.connect(`mongodb://${hostname}:27017/tododb` , { useNewUrlParser: true });
-
+mongoose.connect(process.env.MONGOCONNECTION , { useNewUrlParser: true });
 module.exports = { mongoose }
