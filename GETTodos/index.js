ce7265beb993d7b60c2787f5dd1ca08566e5f1b3
shreeganes <shreevidhya.ganesan@byon8.com>
2018-11-09 10:22:26 +0100

    logging result

--- GETTodos/index.js
+++ GETTodos/index.js
@@ -1,7 +1,11 @@
 const Dao = require('../db/DAO')
 
 module.exports =  async function (context, req) {
-    const responseObject = await Dao.getTodo().then((result) => { return ({ status: 200, body: result }) })
+    const responseObject = await Dao.getTodo().then((result) => {
+         console.log(result)
+         context.log(result)
+         return ({ status: 200, body: result }) 
+        })
         .catch((err) => {
             if (err.code === 404)
                 return ({ status: 404, body: err.body })
