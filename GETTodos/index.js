0425a823ea0705822f19706b52bec4ed0088cd11
shreeganes <shreevidhya.ganesan@byon8.com>
2018-11-09 12:00:38 +0100

    changes

--- GETTodos/index.js
+++ GETTodos/index.js
@@ -2,8 +2,6 @@ const Dao = require('../db/DAO')
 
 module.exports =  async function (context, req) {
     const responseObject = await Dao.getTodo().then((result) => {
-         console.log(result)
-         context.log(result)
          return ({ status: 200, body: result }) 
         })
         .catch((err) => {
