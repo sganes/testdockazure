fc4d57c802e2e40ad2d31491663707baa4754693
shreeganes <shreevidhya.ganesan@byon8.com>
2018-11-12 10:30:20 +0100

    again chages in mongo connection

--- db/mongoose_connect.js
+++ db/mongoose_connect.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const host = process.env.host || 'localhost';
-mongoose.connect(`mongodb://${host}:27017/sampledb`, { useNewUrlParser: true });
+if (process.env.NODE_ENV === 'staging' )
+    mongoose.connect(`mongodb://0.0.0.0:27017/sampledb`, { useNewUrlParser: true });
+else
+    mongoose.connect(`mongodb://localhost:27017/sampledb`, { useNewUrlParser: true });    
 module.exports = { mongoose }
