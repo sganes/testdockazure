f75ce5b4e26ec427015dec5563a84a9d7b756174
shreeganes <shreevidhya.ganesan@byon8.com>
2018-11-12 10:13:29 +0100

    changes to set connectionstring '

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM mcr.microsoft.com/azure-functions/node:2.0
 ENV AzureWebJobsScriptRoot=/home/site/wwwroot
+ENV NODE_ENV staging
 COPY . /home/site/wwwroot
--- welcome/index.js
+++ welcome/index.js
@@ -1,7 +1,6 @@
 module.exports = async function (context, req) {
-    const host = process.env.host || 'localhost';
     context.res = {
         status: 400,
-        body: `Welcome. Mongo host is ${host}`
+        body: "Welcome"
     }
 }
